2157b8dd342e64438d4d2a639a433dc5b0075007
kmal808 <kjmalley7@gmail.com>
2022-11-01 23:49:36 -1000

    removing console logs

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-// document.querySelector('button').addEventListener('click', getFetch)
-
-// function getFetch(){
-//   const link = document.querySelector('input').value
-//   const url = `https://api.shrtco.de/v2/shorten?url=${link}`
-
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data)
-//         document.querySelector('h4').innerText = data.result.full_short_link2
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
-
 const link = document.querySelector('input')
 const url = 'https://www.google.com/search?client=firefox-b-d&q=google+search'
 const obj = {}
@@ -32,6 +15,3 @@ const short = urlShortener(url)
 const original = urlRedirector(short)
 
 document.querySelector('button').addEventListener('click', urlShortener)
-
-console.log(short)
-console.log(original)
